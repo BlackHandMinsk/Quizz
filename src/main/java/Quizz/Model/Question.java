cb32a19f92358bc9fcbadffda372028fe85b5a4c
BlackHandMinsk <3058079@gmail.com>
2021-02-08 10:28:20 +0300

    some questions

--- src/main/java/Quizz/Model/Question.java
+++ src/main/java/Quizz/Model/Question.java
@@ -29,31 +29,6 @@ public class Question {
         this.answerVar = answerVar;
     }
 
-//    public void printAnswer() {
-//        System.out.println("Выберите один вариант овета: ");
-//        for (int i = 0; i < answerVar.length; i++) {
-//            System.out.println(i + 1 + ". " + answerVar[i]);
-//        }
-//    }
-//
-//    public int checkAnswer(String answer) {
-//        if (answer.equals(correctAnswer))
-//            return 1;
-//        else return 0;
-//    }
-//
-//    public String getQuestion() {
-//        return question;
-//    }
-//
-//    public String getCorrectAnswer() {
-//        return correctAnswer;
-//    }
-//
-//    public String[] getAnswerVar() {
-//        return answerVar;
-//    }
-
     @Override
     public String toString() {
         return "Question{" +
