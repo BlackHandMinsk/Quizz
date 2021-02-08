a4a21e3c5fb4fa6165590a0fe6678ae9226b2916
BlackHandMinsk <3058079@gmail.com>
2021-02-08 14:09:59 +0300

    yes or no questions

--- src/main/java/Quizz/Main.java
+++ src/main/java/Quizz/Main.java
@@ -6,6 +6,6 @@ import Quizz.MenuController.SoundController;
 public class Main {
     public static void main(String[] args) {
         new MenuController().start();
-        SoundController.playSound("C:\\Users\\37544\\IdeaProjects\\Quizz\\src\\main\\resources\\1128.wav").play();
+       // SoundController.playSound("C:\\Users\\37544\\IdeaProjects\\Quizz\\src\\main\\resources\\1128.wav").play();
     }
 }
--- src/main/java/Quizz/MenuController/MenuController.java
+++ src/main/java/Quizz/MenuController/MenuController.java
@@ -1,12 +1,12 @@
 package Quizz.MenuController;
 
-import Quizz.DTO.QuestionDto;
+
 import Quizz.DTO.UserDto;
 import Quizz.Exceptions.ItemNotFoundException;
 import Quizz.Exceptions.MistakeException;
 import Quizz.Model.Question;
 import Quizz.Model.Rank;
-import Quizz.Model.User;
+
 import Quizz.Repository.UserRepository;
 import Quizz.Service.QuestionService;
 import Quizz.Service.UserService;
@@ -15,7 +15,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,10 +24,14 @@ public class MenuController {
 
     private static final String QUIZ_LOGO = "\n ╔═══╗╔╗╔╗╔══╗╔═══╗\n ║╔═╗║║║║║╚╗╔╝╚═╗─║\n ║║─║║║║║║─║║──╔╝╔╝\n ║║╔╝║║║║║─║║─╔╝╔╝\n ╚═══╝╚══╝╚══╝╚═══╝";
     private static final String  START_MENU =  "-------------------------\n1. ВОЙТИ В АККАУНТ\n2. СОЗДАТЬ АККАУНТ\ne. ЗАКРЫТЬ ПРОГРАММУ\n-------------------------";
-    private static final String  USER_MENU =  "-------------------------\n1. НОВАЯ ИГРА\n2. ПОСМОТРЕТЬ РЕЙТИНГ\n3. ПОКАЗАТЬ ТАБЛИЦУ ЛИДЕРОВ\ne. ПРЕДЫДУЩЕЕ МЕНЮ\n-------------------------";
+    private static final String  USER_MENU =  "-------------------------\n1. НОВАЯ ИГРА\n2. ИГРА ДА ИЛИ НЕТ\n3. ПОСМОТРЕТЬ РЕЙТИНГ\n4. ПОКАЗАТЬ ТАБЛИЦУ ЛИДЕРОВ\ne. ПРЕДЫДУЩЕЕ МЕНЮ\n-------------------------";
+
+    private static final String YES_OR_NO_QUESTIONS_PATH =  "C:\\Users\\37544\\IdeaProjects\\Quizz\\src\\main\\resources\\yes-or-no-questions.json";
     private static final String QUESTIONS_PATH = "C:\\Users\\37544\\IdeaProjects\\Quizz\\src\\main\\resources\\questions.json";
+
     private final UserService userService = new UserService();
     private final UserRepository userRepository = new UserRepository();
+
     private static final String MISTAKE_SOUND = "C:\\Users\\37544\\IdeaProjects\\Quizz\\src\\main\\resources\\sound-mistake.wav";
     private static final String GAME_OVER_SOUND = "C:\\Users\\37544\\IdeaProjects\\Quizz\\src\\main\\resources\\zvuk-game-over.wav";
     private final File MAIN_MENU_SOUND = new File("C:\\Users\\37544\\IdeaProjects\\Quizz\\src\\main\\resources\\startMenuSound.wav");
@@ -37,7 +41,7 @@ public class MenuController {
 
 
 
-    public void start() {
+    public void start(){
         SoundController soundController = new SoundController(MAIN_MENU_SOUND);
             soundController.setVolume(0.7f);
             soundController.play();
@@ -61,6 +65,7 @@ public class MenuController {
                             soundController.close();
                             break;
                     }
+
                 } catch (IOException e) {
                     System.err.println("НЕВАЛИДНЫЙ ВВОД ДАННЫХ: " + e.getMessage());
                 }
@@ -92,7 +97,6 @@ public class MenuController {
                         }
                     }
                 }
-
             } catch(ItemNotFoundException e){
                 System.err.println("ДАННАЯ УЧЕТНАЯ ЗАПИСЬ НЕ НАЙДЕНА: " + e.getMessage());
             } catch(IOException | SQLException e){
@@ -100,7 +104,6 @@ public class MenuController {
             }
 
         }
-
     }
 
 
@@ -115,20 +118,16 @@ public class MenuController {
             inName = reader.readLine();
             if ("e".equals(inName)) {
                 start();
-            }
-            else {
+            } else {
                 System.out.println("ВВЕДИТЕ ПАРОЛЬ");
                 inPassword = reader.readLine();
                 UserDto userDto = userService.newUser(inName, inScore, inPassword);
                 System.out.println("НОВЫЙ АККАУНТ СОЗДАН, ДОБРО ПОЖАЛОВАТЬ");
                 startUserSession(userDto.getUser(),userDto.getId());
             }
-
         } catch (IOException | SQLException | ItemNotFoundException e) {
             e.printStackTrace();
         }
-
-
     }
 
     private void startUserSession(String user,int id) throws SQLException, ItemNotFoundException {
@@ -143,9 +142,12 @@ public class MenuController {
                         newGame(id);
                         break;
                     case "2":
-                        showRating(id);
+                        yesOrNo(id);
                         break;
                     case "3":
+                        showRating(id);
+                        break;
+                    case "4":
                         showRank(id);
                         break;
                     case "e":
@@ -157,6 +159,7 @@ public class MenuController {
         }
     }
 
+
     private void showRank(int id) throws SQLException {
        ArrayList<Rank> ranks = userRepository.getRank();
         System.out.println("ТАБЛИЦА ЛИДЕРОВ: ");
@@ -165,7 +168,60 @@ public class MenuController {
       }
     }
 
-    private void newGame(int id) throws IOException, SQLException, ItemNotFoundException {
+    private void yesOrNo(int id) throws IOException {
+        int mistake = 0;
+        int level = 0;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Question[]> arr = new ArrayList<>();
+        QuestionService.toJavaObject(arr, YES_OR_NO_QUESTIONS_PATH);
+        try {
+            System.out.println("-----------------------\nДА ИЛИ НЕТ\n-----------------------");
+            System.out.println("ВЫБЕРИТЕ УРОВЕНЬ СЛОЖНОСТИ: \n1. Легкий - 5 ошибок\n2. Средний - 3 ошибки\n3. Тяжелый - 1 ошибка");
+            String in = reader.readLine();
+            switch (in) {
+                case "1":
+                    level+=5;
+                    System.out.println("------------------------\nВЫ ВЫБРАЛИ ЛЕГКИЙ УРОВЕНЬ СЛОЖНОСТИ\n------------------------");
+                    break;
+                case "2":
+                    level+=3;
+                    System.out.println("------------------------\nВЫ ВЫБРАЛИ СРЕДНИЙ УРОВЕНЬ СЛОЖНОСТИ\n------------------------");
+                    break;
+                case "3":
+                    level+=1;
+                    System.out.println("------------------------\nВЫ ВЫБРАЛИ ТЯЖЕЛЫЙ УРОВЕНЬ СЛОЖНОСТИ\n------------------------");
+                    break;
+            }
+            System.out.println("ОТВЕЧАЙТЕ НА ВОПРОСЫ ДА ИЛИ НЕТ, ЗА КАЖДЫЙ ПРАВИЛЬНЫЙ ОТВЕТ ВЫ ПОЛУЧАТЕТЕ ОДИН БАЛЛ\nВЫ МОЖЕТЕ ДОПУСТИТЬ "+level+" ОШИБКИ");
+            for (Question[] question : arr) {
+                for (Question question1 : question) {
+                    System.out.println(question1.getQuestion());
+                    System.out.println(Arrays.toString(question1.getAnswerVar()));
+                    String a = reader.readLine().toLowerCase();
+                    if (!a.equals(question1.getCorrectAnswer().toLowerCase())) {
+                        mistake += 1;
+                        SoundController.playSound(MISTAKE_SOUND).join();
+                        System.out.println("------------------------\nНЕПРАВИЛЬНЫЙ ОТВЕТ\n------------------------");
+                        if(mistake==level){
+                            throw new MistakeException();
+                        }
+                    } else if (a.equals(question1.getCorrectAnswer().toLowerCase())) {
+                        userRepository.scoreUpdate(id);
+                        System.out.println("------------------------\nОТВЕТ ВЕРЕН\n------------------------");
+                        System.out.println(question1.getCorrectAnswerInfo()+"\n------------------------");
+                    }
+                }
+            }
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        } catch (MistakeException c) {
+            System.out.println("------------------------\nНЕПРАВИЛЬНЫЙ ОТВЕТ, ИГРА ЗАКОНЧЕНА\n------------------------");
+            SoundController.playSound(GAME_OVER_SOUND).join();
+        }
+    }
+
+    private void newGame(int id) throws IOException, SQLException {
         int mistake = 0;
         int level = 0;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
